3fa62584c54208912585f91f12990faa66e98ade
takosuke <solidparallel6890@gmail.com>
2025-07-11 17:41:51 +0200

    moved gitsigns to module

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -34,19 +34,10 @@ require('lazy').setup({
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
   --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- modular approach: using `require 'path.name'` will
+  -- include a plugin definition from file lua/path/name.lua
+
+  require 'kickstart.plugins.gitsigns', -- See `:help gitsigns` to understand what the configuration keys do
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
