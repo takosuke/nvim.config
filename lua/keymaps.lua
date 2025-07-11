afa5d80b24e50d4a40d0192abfb84d8f5d6e5af8
takosuke <solidparallel6890@gmail.com>
2025-07-11 17:26:37 +0200

    mapped semicolon to colon

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Map semicolon to colon
+vim.keymap.set('n', ';', ':')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
